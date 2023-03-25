9e80b5acf273bff44c9af82d9c7e7a3e6ae00fe2
Tim <tim.91.lindstrom@gmail.com>
2023-03-25 12:51:00 +0100

    Added start screen and animation screens. Also added navigation to the animation screens from start screen

--- iOS-Animation/ContentView.swift
+++ iOS-Animation/ContentView.swift
@@ -10,76 +10,45 @@ import CoreData
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
+    
+    /*@FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+        animation: .default)*/
+    
+    @State var apiAnimation = false
+    @State var customAnimation = false
+    
     var body: some View {
         NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item attttttt \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
+            VStack{
+                Button{
+                    apiAnimation.toggle()
+                } label: {
+                    Text("API")
                 }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
+                Button{
+                    customAnimation.toggle()
+                } label: {
+                    Text("Custom")
                 }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
+                
+                NavigationLink(isActive: $apiAnimation){
+                    ApiAnimationView()
+                } label:{
+                    EmptyView()
                 }
-            }
-            Text("Select an item")
+                NavigationLink(isActive: $customAnimation){
+                    CustomAnimationView()
+                } label:{
+                    EmptyView()
+                }
+                
+            }.navigationBarTitle(Text("Animation Experiment"))
         }
     }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
+    
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ iOS-Animation/CustomAnimationView.swift
@@ -0,0 +1,20 @@
+//
+//  CustomAnimationView.swift
+//  iOS-Animation
+//
+//  Created by Sabina Tim on 2023-03-25.
+//
+
+import SwiftUI
+
+struct CustomAnimationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CustomAnimationView_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomAnimationView()
+    }
+}
